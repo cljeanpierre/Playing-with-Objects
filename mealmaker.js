4b9c379ed31201f8c91c6d879bfedb6feffcaaaf
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2021-08-03 11:51:12 -0400

    created setter methods with required parameters named specifically after 1 item from each course

--- mealmaker.js
+++ mealmaker.js
@@ -6,6 +6,7 @@ const menu = {
       mains: [],
       desserts: [],
     },
+
     get appetizers() {
         return this._courses.appetizers;
     },
@@ -14,5 +15,15 @@ const menu = {
     },
     get desserts() {
         return this._courses.desserts;
+    },
+
+    set appetizers(appetizer) {
+        this._courses.appetizers = appetizer;
+    },
+    set mains(maincourse) {
+        this._courses.mains = maincourse;
+    },
+    set desserts(dessert) {
+        this._courses.desserts = dessert;
     },
 };

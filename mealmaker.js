c5f39b98e063defbe60ee5101fd300ab1d25a1ee
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2021-08-03 11:38:05 -0400

    created empty arrays to hold items from 3 courses; created 3 getter methods to return the properties from each course

--- mealmaker.js
+++ mealmaker.js
@@ -2,7 +2,17 @@
 
 const menu = {
     _courses: {
-
+      appetizers: [],
+      mains: [],
+      desserts: [],
     },
-
+    get appetizers() {
+        return this._courses.appetizers;
+    },
+    get mains() {
+        return this._courses.mains;
+    },
+    get desserts() {
+        return this._courses.desserts;
+    },
 };

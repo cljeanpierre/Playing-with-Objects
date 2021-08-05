4c061be67d57ff4f66cba83aad7edfcbff5e5557
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2021-08-05 17:58:33 -0400

    added function to generate a random meal from all three courses

--- mealmaker.js
+++ mealmaker.js
@@ -49,4 +49,12 @@ const menu = {
         return dishes[randomIndex];
     },
 
+    generateRandomMeal() {
+        const appetizer = this.getRandomDishFromCourse('appetizers');
+        const main = this.getRandomDishFromCourse('mains');
+        const dessert = this.getRandomDishFromCourse('desserts');
+        const totalPrice = appetizer.price + main.price + dessert.price;
+        return `Your meal is ${appetizer.name}, ${meal.name}, and ${dessert.name}, and the total price ${totalPrice}`;
+    }
 };
+
